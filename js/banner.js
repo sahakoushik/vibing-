7879cbade27ae719b566f797b3632fd80d58d78a
Saha Koushik Kumar <sahakoushikkumar@gmail.com>
2019-01-12 12:58:19 +0600

    Add files via upload
    initial commit

---
+++ js/banner.js
@@ -0,0 +1,49 @@
+﻿window.addEventListener('load', function () {
+
+    var four = document.querySelector('.four');
+    var five = document.querySelector('.five');
+    var six = document.querySelector('.six');
+    var seven = document.querySelector('.seven');
+    delay = 500;
+
+    setTimeout(function () {
+        $('.content-2').addClass('loaded');
+    }, 2000);
+
+    var animation = function () {
+        setTimeout(function () {
+            four.style.top = '0%';
+        }, delay * 2);
+        setTimeout(function () {
+            four.style.top = '100%';           
+        }, delay * 5);
+
+        setTimeout(function () {
+            var animationDelay = delay *5 
+            five.style.top = '0%';
+            //five.style.animation = "fadein 3s";
+            //$('#five').css({ 'animation-delay': animationDelay });
+        }, delay * 6);
+        setTimeout(function () {
+            five.style.top = '100%';
+        }, delay * 11);
+
+        setTimeout(function () {
+            six.style.top = '0%';
+            //six.style.animation = "fadein 4s";
+        }, delay * 12);
+        setTimeout(function () {
+            six.style.top = '100%';
+        }, delay * 17);
+
+        setTimeout(function () {
+            seven.style.top = '0%';
+            //seven.style.animation = "fadein 5s";
+        }, delay * 18);
+        setTimeout(function () {
+            seven.style.top = '100%';
+        }, delay * 23);
+    };
+    animation();
+    setInterval(animation, delay * 24);
+});
---
+++ js/main.js
@@ -0,0 +1,41 @@
+﻿$(function () {
+
+    $('#myCarousel').carousel({
+        interval: 2000
+    });
+
+    // handles the carousel thumbnails
+    $('[id^=carousel-selector-]').click(function () {
+        var id_selector = $(this).attr("id");
+        var id = id_selector.substr(id_selector.length - 1);
+        id = parseInt(id);
+        $('#myCarousel').carousel(id);
+        $('[id^=carousel-selector-]').removeClass('selected');
+        $(this).addClass('selected');
+    });
+
+    var is_start = true;
+    // when the carousel slides, auto update
+    $(document).on('slid', '#myCarousel', function () {
+        //console.log('lol ');
+        var id = $('.item.active').data('slide-number') + 1;
+        //if(!is_start){
+        //    id = id + 1;
+        //}
+
+        if (id > 4) {
+            id = 0
+        }
+        id = parseInt(id);
+        $('[id^=carousel-selector-]').removeClass('selected');
+        $('[id=carousel-selector-' + id + ']').addClass('selected');
+        console.log(id);
+        //is_start = false;
+    });
+
+    $('#myCarousel').on('slide.bs.carousel', function () {
+        //console.log('lol ');
+
+        $('#myCarousel').trigger('slid');
+    })
+});
